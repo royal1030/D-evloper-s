7903f105ee0d1d5a9461da2a44428079bb1e866f
sharma-gautam1234 <gautamdadhich001@gmail.com>
2023-04-05 23:22:39 +0530

    card data updated

--- ARG-COLLAB/src/pages/job.jsx
+++ ARG-COLLAB/src/pages/job.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Job () {
      return (
         <>
-
+        <h1>JOB</h1>
         </>
      )
 };
--- ARG-COLLAB/src/puppeteer_indeed.js
+++ ARG-COLLAB/src/puppeteer_indeed.js
@@ -1,57 +1,76 @@
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
 
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   await page.goto("https://www.indeed.com/jobs?q=software+engineer&l=USA");
+
+//   const jobs = await page.evaluate(() => {
+//     const jobList = [];
+//     const jobElements = document.querySelectorAll(
+//       "#job_b366fa469fd520b4 span"
+//     );
+
+//     // for (const jobElement of jobElements) {
+//     //   const title = jobElement.querySelector('.title a').innerText;
+//     //   const company = jobElement.querySelector('.company').innerText;
+//     //   const location = jobElement.querySelector('.location').innerText;
+//     //   const salary = jobElement.querySelector('.salaryText')?.innerText || '';
+
+//     //       jobList.push({ title, company, location, salary });
+//     //     }
+
+//     //     return jobList;
+//     //   });
+//     jobElements.forEach((tag3) => {
+//       jobList.push(tag3.innerText);
+//     });
+//     return jobList;
+//   });
+
+//   console.log(jobs);
+
+//   await browser.close();
+// })();
+
+// _______________________________________________ANISH__________________________________________________________
+
+// const puppeteer = require('puppeteer');
 // const fs = require('fs');
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto("https://in.indeed.com/jobs?q=internship&l=Work+from+Home&from=searchOnHP&vjk=3e9ab0fd3fc17f63");
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto('https://www.indeed.com/jobs?q=internship&l=United+States');
 
-  // Extract the data
-  const data1 = await page.evaluate(() => {
-    const elements1 = document.querySelectorAll(".jcs-JobTitle.css-jspxzf.eu4oa1w0 a span");
-    const technologies1 = [];
-    elements1.forEach((tag1) => {
-      technologies1.push(tag1.innerHTML);
-    });
-    return technologies1;
-  });
-//   const data2 = await page.evaluate(() => {
-//     const elements2 = document.querySelectorAll("#news a p");
-//     const technologies2 = [];
-//     elements2.forEach((tag2) => {
-//       technologies2.push(tag2.innerText);
+//   const jobs = await page.evaluate(() => {
+//     const jobNodes = document.querySelectorAll('.jobsearch-SerpJobCard');
+//     const jobList = [];
+
+//     jobNodes.forEach((node) => {
+//       const title = node.querySelector('.jobtitle').textContent.trim();
+//       const company = node.querySelector('.company').textContent.trim();
+//       const location = node.querySelector('.location').textContent.trim();
+//       const link = node.querySelector('.jobtitle').getAttribute('href');
+//       const summary = node.querySelector('.summary').textContent.trim();
+
+//       jobList.push({
+//         title,
+//         company,
+//         location,
+//         link,
+//         summary,
+//       });
 //     });
-//     return technologies2;
-//   });
-//   const data3 = await page.evaluate(() => {
-//     const elements3 = document.querySelectorAll("#notices a p");
-//     const technologies3 = [];
-//     elements3.forEach((tag3) => {
-//       technologies3.push(tag3.innerText);
-//     });
-//     return technologies3;
+
+//     return jobList;
 //   });
 
-//   const arr = [data1, data2, data3];f
-// Your Puppeteer code to scrape the data
-// const scrapedData = {
-//   // your scraped data object
-//   data1,
-//   data2,
-//   data3
-// };
+//   fs.writeFile('indeed_jobs.json', JSON.stringify(jobs, null, 2), (err) => {
+//     if (err) throw err;
+//     console.log('Jobs saved to file!');
+//   });
 
-// fs.writeFile('scraped-data.json', JSON.stringify(scrapedData), err => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log('Scraped data has been written to scraped-data.json');
-// });
-  console.log(data1);
-//   console.log(data2);
-//   console.log(data3);
-
-  await browser.close();
-})();
+//   await browser.close();
+// })();
